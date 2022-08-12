4a90f076fe2c3dd71bb7534d4507db2d1cf06a22
Chuanhao LI <chuanhaoli@ChuanhaodeMacBook-Pro.local>
2022-08-12 09:37:08 +0800

    completed and verified all functions
    added comments, *trav in check function changed to a variable

--- lecture5_notes/speller/dictionary.c
+++ lecture5_notes/speller/dictionary.c
@@ -33,17 +33,20 @@ bool check(const char *word)
     unsigned int hash_number;
     // Get hash value of the word
     hash_number = hash(word);
-    node *trav = NULL;
-    trav = table[hash_number]->next;
+    // Set up a trav pointer
+    node *trav = table[hash_number];
+    // Loop until trav points to NULL
     while (trav != NULL)
     {
+        // Compare the word with trav->word in linked list
         if (strcasecmp(trav->word, word) == 0)
         {
             return true;
         }
+        // Move trav to the next node
         trav = trav->next;
     }
-    
+    // If the function does not return in while loop, indicates no such word in dictionary
     return false;
 }
 
@@ -60,17 +63,20 @@ bool load(const char *dictionary)
     // TODO
     // Open dictionary file
     FILE *fp = fopen(dictionary, "r");
+    // Check NULL
     if (fp == NULL)
     {
         return false;
     }
     
+    // Create a buffer to store information
     char buffer[LENGTH + 1];
     
     // Read words from dictionary
     while (fscanf(fp, "%s", buffer) != EOF)
     {
         node *n = malloc(sizeof(node));
+        // Check NULL
         if (n == NULL)
         {
             return false;
@@ -84,27 +90,33 @@ bool load(const char *dictionary)
         hash_number = hash(n->word);
 
         // Insert a new node
+        // If the word is the first word in the hash-table linked list
         if (table[hash_number] == NULL)
         {
             table[hash_number] = n;
             n->next = NULL;
+            // Increase the counter in order to simplify the size function
             word_counter++;
         }
         else
         {
             n->next = table[hash_number];
             table[hash_number] = n;
+            // Increase the counter in order to simplify the size function
             word_counter++;
         }
     }
+    // Close file
     fclose(fp);
-    return false;
+    // Successfully loaded
+    return true;
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
     // TODO
+    // Simply return word_counter
     return word_counter;
 }
 
@@ -115,18 +127,23 @@ bool unload(void)
     node *trav = NULL;  
     node *temp = NULL;  
     for (int i = 0; i < N; i++)
-    {
+    {   
+        // If hash table index i has values in it
         if (table[i] != NULL)
         {
             trav = table[i];  
             do
             {
+                // Let temp = trav in order to memorize the location of current node
                 temp = trav;
+                // Move trav forward to point to the next node 
                 trav = trav->next;
+                // Free temp
                 free(temp);
+                // Notice: Now trav is pointing to one location forward, free(temp) will not affect rest of the list
             } while (trav != NULL);
         }
     }
-    // How do I know if it is successful
+    // Successfully unloaded
     return true;
 }
